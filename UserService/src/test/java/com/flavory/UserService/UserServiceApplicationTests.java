91b603824bf0f51d257378ef7934b274aaebece4
Kamil Mieczkowski <kamil.mieczkowski7@gmail.com>
2025-11-11 21:20:49 +0100

    configuration of docker-compose and the first user service microservice

---
+++ UserService/src/test/java/com/flavory/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavory.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
