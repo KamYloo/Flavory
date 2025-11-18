5073941714a23fe5b8b26496d80603d7c05da18b
Kamil Mieczkowski <kamil.mieczkowski7@gmail.com>
2025-11-18 10:55:21 +0100

    Fixed a bug with the column - adding the auth0_id name

--- UserService/src/main/java/com/flavory/userservice/entity/User.java
+++ UserService/src/main/java/com/flavory/userservice/entity/User.java
@@ -28,7 +28,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 100)
+    @Column(name = "auth0_id", nullable = false, unique = true, length = 100)
     private String auth0Id;
 
     @Column(nullable = false, unique = true, length = 100)
